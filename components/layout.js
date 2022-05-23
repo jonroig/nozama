def860e734eee2e22f6f25eebbe0b60bfca04ea0
Jon Roig <jon@jonroig.com>
2022-05-22 19:16:24 -0700

    analytics

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,21 @@
+import Script from 'next/script';
+
 import Navbar from './navbar';
 import Footer from './footer';
+import config from '../config';
 
 export default function Layout({ children }) {
     return (
       <>
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${config.gtag}`} strategy="afterInteractive"/>
+        <Script strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${config.gtag}');`}
+        </Script>
+        
         <Navbar />
         <div id="fb-root"></div>
         <main>{children}</main>
--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const config = {
-    affiliateId: 'nozama072-20'
+    affiliateId: 'nozama072-20',
+    gtag: 'G-PNW5ZJ3ENG'
 };
 
 export default config;
