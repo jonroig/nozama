5d802ff348751162510d9a024b61a9785d9baf70
Jon Roig <jon@jonroig.com>
2022-05-26 09:42:25 -0700

    still messing with manifest

--- pages/amazonpurchasehistory.js
+++ pages/amazonpurchasehistory.js
@@ -13,7 +13,7 @@ function Amazonpurchasehistory() {
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="manifest" href="/manifest.json" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@_nozama_" />
           <meta name="twitter:creator" content="@runnr_az" />
